3583cd922c32412dd7929b6906308c17b4bdd44a
Repozotorii <48119000+Repozotorii@users.noreply.github.com>
2021-08-11 11:50:28 +0600

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,345 @@
+<?php include 'contactus/handling_form.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="TemplateMo">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700" rel="stylesheet">
+
+    <title>КОНТАКТЫ</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+    <link rel="stylesheet" href="assets/scss/style.css">
+
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="lib/flaticon/font/flaticon.css" rel="stylesheet">
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+    <link href="lib/slick/slick.css" rel="stylesheet">
+    <link href="lib/slick/slick-theme.css" rel="stylesheet">
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+    <link rel="stylesheet" href="Главная.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <!--
+
+Host Cloud Template
+
+https://templatemo.com/tm-541-host-cloud
+
+-->
+</head>
+
+<body>
+
+    <div class="top-bar hide-for-mobile">
+        <div class="container-fluid">
+            <div class="row align-items-center">
+                <div class="col-lg-4 col-md-12">
+                    <div class="logo">
+                        <a href="index.html">
+                            <h1>ТОО "PREMIUM АУДИТ"</h1>
+                            <!-- <img src="img/logo.jpg" alt="Logo"> -->
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-8 col-md-7 d-none d-lg-block">
+                    <div class="row">
+                        <div class="col-4">
+                            <div class="top-bar-item">
+                                <div class="top-bar-icon">
+                                    <i class="flaticon-calendar"></i>
+                                </div>
+                                <div class="top-bar-text">
+                                    <h3>Часы Работы</h3>
+                                    <p>Пн - Пт, 09:00 - 17:00</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="top-bar-item">
+                                <div class="top-bar-icon">
+                                    <i class="flaticon-call"></i>
+                                </div>
+                                <div class="top-bar-text">
+                                    <h3>Свяжитесь с Нами</h3>
+                                    <p>+7-(776)-017-93-39</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="top-bar-item">
+                                <div class="top-bar-icon">
+                                    <i class="flaticon-send-mail"></i>
+                                </div>
+                                <div class="top-bar-text">
+                                    <h3>Напишите Нам</h3>
+                                    <p>tooaudit@gmail.com</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <header class="u-clearfix u-header u-header" id="sec-948d">
+        <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+            <a href="index.html" class="u-image u-logo u-image-1">
+
+            </a>
+            <nav class="u-menu u-menu-dropdown u-offcanvas u-menu-1">
+                <div class="menu-collapse" style="font-size: 1rem; letter-spacing: 0px;">
+                    <a class="u-button-style u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-top-bottom-menu-spacing u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#">
+                        <svg>
+        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#menu-hamburger"></use>
+      </svg>
+                        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <defs>
+          <symbol id="menu-hamburger" viewBox="0 0 16 16" style="width: 16px; height: 16px;">
+            <rect y="1" width="16" height="2"></rect>
+            <rect y="7" width="16" height="2"></rect>
+            <rect y="13" width="16" height="2"></rect>
+          </symbol>
+        </defs>
+      </svg>
+                    </a>
+                </div>
+                <div class="u-nav-container">
+                    <ul class="u-nav u-unstyled u-nav-1">
+                        <li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="index.html" style="padding: 10px 20px;">Главная</a>
+                        </li>
+                        <li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="about.html" style="padding: 10px 20px;">О нас</a>
+                        </li>
+                        <li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="services.html" style="padding: 10px 20px;">Услуги</a>
+                        </li>
+                        <li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="contact.php" style="padding: 10px 20px;">Контакты</a>
+                        </li>
+                        <li class="u-nav-item">
+                            <div class="language">
+                                <img src="assets/images/lang/lang/lang__ru.png" alt="ru" data-google-lang="ru" class="language__img">
+                                <img src="assets/images/lang/lang/lang__en.png" alt="en" data-google-lang="en">
+                                <img src="assets/images/lang/lang/lang__de.png" alt="de" data-google-lang="de" class="language__img">
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="u-nav-container-collapse">
+                    <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+                        <div class="u-sidenav-overflow">
+                            <div class="u-menu-close"></div>
+                            <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2">
+                                <li class="u-nav-item"><a class="u-button-style u-nav-link" href="index.html">Главная</a>
+                                </li>
+                                <li class="u-nav-item"><a class="u-button-style u-nav-link" href="about.html">О нас</a>
+                                </li>
+                                <li class="u-nav-item"><a class="u-button-style u-nav-link" href="services.html">Услуги</a>
+                                </li>
+                                <li class="u-nav-item"><a class="u-button-style u-nav-link" href="contact.php">Контакты</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+                </div>
+            </nav>
+        </div>
+    </header>
+
+    <!-- Page Content -->
+
+    <!-- Heading Starts Here -->
+    <div class="page-heading header-text">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>Контакты</h1>
+                    <p><a href="index.html">Главная</a> / <span>Контакты</span></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Heading Ends Here -->
+
+
+    <!-- Contact Us Starts Here -->
+    <div class="contact-us">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="contact-form">
+                    <?= $result ?>
+                        <form id="contact" action="" method="post">
+                            <div class="row">
+                                <div class="col-md-12 col-sm-12">
+                                    <fieldset>
+                                        <input name="name" type="text" id="name" placeholder="Ваше имя" required="">
+                                    </fieldset>
+                                </div>
+                                <div class="col-md-12 col-sm-12">
+                                    <fieldset>
+                                        <input name="email" type="text" id="email" placeholder="Ваш email" required="">
+                                    </fieldset>
+                                </div>
+                                <div class="col-md-12 col-sm-12">
+                                    <fieldset>
+                                        <input name="tema" type="text" id="tema" placeholder="Тема">
+                                    </fieldset>
+                                </div>
+                                <div class="col-lg-12">
+                                    <fieldset>
+                                        <textarea name="message" rows="6" id="message" placeholder="Ваше сообщение" required=""></textarea>
+                                    </fieldset>
+                                </div>
+                                <div class="col-lg-12">
+                                    <fieldset>
+                                        
+                                        <button type="submit" id="form-submit" class="main-button" name="submit">Отправить</button>
+                                    </fieldset>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="right-content">
+                        <div class="section-heading">
+                            <span>Свяжитесь С Нами</span>
+                            <h2>Будем на связи</h2>
+                            <p>У Вас есть вопрос относительно наших услуг? Заполните свои данные, и мы Вам обязательно ответим!</p>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6 col-sm-12">
+                                <ul>
+                                    <li><i class="fa fa-phone"></i>+7-(776)-017-93-39</li>
+                                </ul>
+                            </div>
+                            <div class="col-md-6 col-sm-12">
+                                <ul>
+                                    <li><i class="fa fa-envelope"></i> tooaudit@gmail.com</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Contact Us Ends Here -->
+    <div class="box-up">
+        <p>suceess!!!</p>
+    </div>
+   
+
+    <!-- Map Starts Here -->
+    <div id="map">
+
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1493.8620289043067!2d82.62444017788069!3d49.94797273844973!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x42eb4f55e0955fc7%3A0xbd87e46409865fff!2z0YPQuy4g0JPQvtGA0YzQutC-0LPQviA1Nywg0KPRgdGC0Ywt0JrQsNC80LXQvdC-0LPQvtGA0YHQuiAwNzAwMDA!5e0!3m2!1sru!2skz!4v1614935044165!5m2!1sru!2skz"
+            width="100%" height="520px" frameborder="0" style="border:0" allowfullscreen></iframe>
+    </div>
+
+
+
+    <!-- Testimonials Starts Here -->
+
+    <!-- Testimonials Ends Here -->
+
+
+    <!-- Footer Starts Here -->
+    <div class="footer wow fadeIn" data-wow-delay="0.3s">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-lg-3">
+                    <div class="footer-contact">
+                        <h2>Контакты</h2>
+                        <p><i class="fa fa-map-marker-alt"></i>Добролюбова, д. 34/2<br>г.Усть-Каменогорск</p>
+                        <p><i class="fa fa-phone-alt"></i>+7-(776)-017-93-39</p>
+                        <p><i class="fa fa-envelope"></i>tooaudit@gmail.com</p>
+                        <div class="footer-social">
+                            <a href=""><i class="fab fa-instagram"></i></a>
+                            <a href=""><i class="fab fa-telegram"></i></a>
+                            <a href=""><i class="fab fa-whatsapp"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="footer-link">
+                        <h2>Услуги</h2>
+                        <a href="audit.html">Аудит</a>
+                        <a href="buhuchet.html">Бухгалтерский учет</a>
+                        <a href="outsorsing.html">Аутсорсинг</a>
+                        <a href="naluchet.html">Налоговый учёт</a>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="footer-link">
+                        <h2>Страницы</h2>
+                        <a href="index.html">Главная</a>
+                        <a href="about.html">О Компании</a>
+                        <a href="services.html">Услуги</a>
+                        <a href="contact.php">Контакты</a>
+
+                    </div>
+                </div>
+                <div class="col-lg-3">
+
+                    <img class="img_logo" src="assets/images/ex333.png">
+
+
+                </div>
+            </div>
+        </div>
+
+        <div class="container copyright">
+            <div class="row">
+                <div class="col-md-6">
+                    <p>&copy; <a href="#"> 2021 ТОО "PREMIUM АУДИТ"</a>, Все права защищены.</p>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!-- Footer Ends Here -->
+    
+
+  
+
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/functions.js"></script>
+    <!-- Additional Scripts -->
+    <script src="assets/js/custom.js"></script>
+    <script src="assets/js/owl.js"></script>
+    <script src="assets/js/accordions.js"></script>
+    <script src="assets/js/script.js"></script>
+    <script src="assets/js/google-translate.js"></script>
+    <script src="//translate.google.com/translate_a/element.js?cb=TranslateInit"></script>
+    <script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
+    <script src="assets/js/sendmessage.js"></script>
+    <script language="text/Javascript">
+        cleared[0] = cleared[1] = cleared[2] = 0; //set a cleared flag for each field
+        function clearField(t) { //declaring the array outside of the
+            if (!cleared[t.id]) { // function makes it static and global
+                cleared[t.id] = 1; // you could use true and false, but that's more typing
+                t.value = ''; // with more chance of typos
+                t.style.color = '#fff';
+            }
+        }
+    </script>
+</body>
+
+</html>
